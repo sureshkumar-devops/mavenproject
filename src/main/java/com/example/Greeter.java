675c56b5c665722f85196e645a863db3da9b9c12
sureshkumar-devops <suresh.ak47gmail.com>
2023-06-21 13:01:14 +0530

    Update pom and jave main

--- src/main/java/com/example/Greeter.java
+++ src/main/java/com/example/Greeter.java
@@ -14,13 +14,10 @@ public class Greeter {
 
   
   //TODO: Add javadoc comment
+  
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 
   
-  public static void main(String args[]){
-    
-	  System.out.println("Hello from Main Method!!!");
-  }
 }
